c1978a67352a3bfe4020653bfffc190337dd8fea
okxooxoo <okxooxoo@naver.com>
2024-02-14 16:57:02 +0900

    ✨ feat: 홈 화면 UI 구현

---
+++ src/api/api.js
@@ -0,0 +1,61 @@
+export const getDatas = () => {
+    return Promise.resolve(
+        [
+            {
+                "image": "images/image1.png",
+                "title": "전시회 제목",
+                "place": "그라운드 시소 센트럴( 장소 이름 )",
+                "price": 5000,
+                "period": "2023.07.24 ~ 2023.08.28"
+            },
+        
+            {
+                "image": "images/image2.png",
+                "title": "전시회 제목",
+                "place": "그라운드 시소 센트럴( 장소 이름 )",
+                "price": 5000,
+                "period": "2023.07.24 ~ 2023.08.28"
+            },
+        
+            {
+                "image": "images/image3.png",
+                "title": "전시회 제목",
+                "place": "그라운드 시소 센트럴( 장소 이름 )",
+                "price": 5000,
+                "period": "2023.07.24 ~ 2023.08.28"
+            },
+        
+            {
+                "image": "images/image4.png",
+                "title": "전시회 제목",
+                "place": "그라운드 시소 센트럴( 장소 이름 )",
+                "price": 5000,
+                "period": "2023.07.24 ~ 2023.08.28"
+            },
+        
+            {
+                "image": "images/image5.png",
+                "title": "전시회 제목",
+                "place": "그라운드 시소 센트럴( 장소 이름 )",
+                "price": 5000,
+                "period": "2023.07.24 ~ 2023.08.28"
+            },
+        
+            {
+                "image": "images/image6.png",
+                "title": "전시회 제목",
+                "place": "그라운드 시소 센트럴( 장소 이름 )",
+                "price": 5000,
+                "period": "2023.07.24 ~ 2023.08.28"
+            },
+        
+            {
+                "image": "images/image7.png",
+                "title": "전시회 제목",
+                "place": "그라운드 시소 센트럴( 장소 이름 )",
+                "price": 5000,
+                "period": "2023.07.24 ~ 2023.08.28"
+            }
+        ]
+    );
+};
---
+++ src/screens/home.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import BottomNavBar from "../components/BottomNavBar";
+import ExhibitionCard from "../components/ExhibitionCard";
+import '../styles/Home.css';
+import { getDatas } from '../api/api';
+
+function Home() {
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    const fetchDatas = async () => {
+      const datas = await getDatas();
+      setDatas(datas);
+    };
+
+    fetchDatas();
+  }, []);
+
+  return (
+    <div>
+      <div className='body'>
+        {datas.map((data) => (
+          <ExhibitionCard
+            image={data.image}
+            title={data.title}
+            place={data.place}
+            price={data.price}
+            period={data.period}
+          />
+        ))}
+      </div>
+      <BottomNavBar />
+    </div>
+  );
+}
+
+export default Home;
